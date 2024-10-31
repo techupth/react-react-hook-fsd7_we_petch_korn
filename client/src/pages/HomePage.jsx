61bc3ea0e81492723a91c673fbe56221931b32d8
Arkorn Pattanavibul <arkorn.pat@gmail.com>
2024-10-31 13:22:09 +0700

    feat: add custom hook

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,29 +1,10 @@
-import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
+import { useBlogPosts } from "../hook/useBlogPosts";
 
 function HomePage() {
   const navigate = useNavigate();
 
-  const [posts, setPosts] = useState([]);
-  const [isError, setIsError] = useState(null);
-  const [isLoading, setIsLoading] = useState(null);
-
-  const getPosts = async () => {
-    try {
-      setIsError(false);
-      setIsLoading(true);
-      const results = await axios("http://localhost:4000/posts");
-      setPosts(results.data.data);
-      setIsLoading(false);
-    } catch (error) {
-      setIsError(true);
-    }
-  };
-
-  useEffect(() => {
-    getPosts();
-  }, []);
+ const {posts, isError, isLoading} = useBlogPosts()
 
   return (
     <div>
